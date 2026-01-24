4b875106411a5d2cf3b5d4625974fd5fcd4e4e2e
FaridGitPro <faridbusinessandco@gmail.com>
2026-01-24 12:38:39 -0500

    initial commit

---
+++ js/translations.js
@@ -0,0 +1,151 @@
+const translations = {
+    en: {
+        /**Home Sec **/
+        trHome : "Home",
+        trAbout : "About",
+        trSkills : "Skills",
+        treducation: "Education",
+        trContact : "Contact",
+        trHomeh1 : "Hi I am Farid Bouda",
+        trHomeh3 : "Frontend Developer",
+        trHomep1 : "Experienced Software Developer with 4 years of experience in programming, software and data management based on specific requirements. Adaptable, diligent and bilingual(English and French)",
+        trHomeHireMebtn : "Hire me",
+        trHomeLetsTalkbtn : "Let's Talk",
+        trHomespan: "Farid Bouda",
+        /**About section **/
+        trAbouth2 : "About",
+        trAboutspanMe: "Me",
+        trAbouth3 : "Frontend Developer!",
+        trAboutp : "Experienced Software Developer with 4 years of experience in programming, software and data management based on specific requirements. Adaptable, diligent and bilingual(English and French)",
+        trAboutReadMe : "Read more",
+        /**Education section **/
+        trJourneyh2: "My",
+        trSpanJourney: "journey",
+        treducation: "Education",
+        treducationXP: "Experience",
+        trEducationh3: "Master degree",
+        trEducationh3_1: "Bachelor Degree",
+        trEducationp1: "I Studied at Ottawa university with major program software engineering and minor program in Entreprenariat",
+        trEducationp1_1: "I Studied at Ottawa university with major program software engineering and minor program in Entreprenariat",
+        /**Experience section**/
+        trExph3: "Developer",
+        trExph3_1: "Data Analyst",
+        trExpp1: "3 years experience as a software developer at Shared Service Canada (10/2021 – 05/2024).",
+        trExp1_1: "Developer",
+        trExpp2: "4 months internship as a data analyst at Ottawa University (01/2021 – 04/2021)",
+        /**Coding Skills Section **/
+        trCodingSkillh2My:"My",
+        trCodingSkillh2:"Skills",
+        trCodingSkillh3:"Coding Skills",
+        /**Experience Skills Section **/
+        trExpSkill:"Experience skills",
+        trExpSkill1:"Data visualisation",
+        trExpSkill2:"Automation",
+        trExpSkill3:"Interface and Database management",
+        trExpSkill4:"Entrepreneurship",
+        /**Contact Section **/
+        trContacth2: "Contact",
+        trContacth2Me: "Me",
+        trContactFullName: "Full Name",
+        trContactEmail:"Email address",
+        trContactMobile:"Mobile number",
+        trContactSubject:"Subject",
+        trContactMessage:"Your Message",
+        trContactSubmitbtn:"Submit",
+        /**Footer Section **/
+        trFooterCopyright: "Copyright &copy; 2025 by Farid Bouda | All right reserved.",
+
+
+    },
+
+    fr: {
+        /**Home Section **/
+        trHome : "Accueil",
+        trAbout : "À propos",
+        trSkills : "Compétences",
+        treducation: "Éducation",
+        trContact : "Nous joindre",
+        trHomeh1 : "Salut je suis Farid Bouda",
+        trHomeh3 : "Développeur Frontend",
+        trHomep1 : "Développeur logiciel expérimenté avec 4 ans d'expérience en programmation, gestion de logiciels et de données basées sur des exigences spécifiques. Adaptable, assidu et bilingue(anglais et français)",
+        trHomeHireMebtn : "Embaucher",
+        trHomeLetsTalkbtn : "Discutons",
+        /**About section **/
+        trAbouth2 : "À propos de",
+        trAboutspanMe: "Moi",
+        trAbouth3 : "Développeur Frontend",
+        trAboutp : "Développeur logiciel expérimenté avec 4 ans d'expérience en programmation, gestion de logiciels et de données basées sur des exigences spécifiques. Adaptable, assidu et bilingue(anglais et français)",
+        trAboutReadMe : "Savoir plus ",
+        /**Education Section **/
+        trJourneyh2: "Mon",
+        trSpanJourney: "parcours",
+        treducation: "Éducation",
+        treducationXP: "Expérience",
+        trEducationh3: "Maîtrise en logiciel",
+        trEducationh3_1: "Baccalauréat en logiciel",
+        trEducationp1: "Études à l'université d'Ottawa avec un programme principal en génie logiciel et un programme mineur en entrepreneuriat.",
+        trEducationp1_1: "Études à l'université d'Ottawa avec un programme principal en génie logiciel et un programme mineur en entrepreneuriat.",
+        /**Experience Section **/
+        trExph3: "Développeur",
+        trExpp1: "3 ans d'expérience en tant que développeur de logiciels au Service partagés Canada (10/2021 – 05/2024).",
+        trExph3_1: "Analyst de données",
+        trExpp2: "Experience de plusieurs mois en tant qu'analyste de données à l'Université d'Ottawa (01/2021 – 04/2021)",
+        /**Coding Skills Section **/
+        trCodingSkillh2My:"Mes",
+        trCodingSkillh2:"compétences",
+        trCodingSkillh3:"Compétences en codage",
+        /**Experience Skills Section **/
+        trExpSkill:"Expérience",
+        trExpSkill1:"Visualisation de données",
+        trExpSkill2:"Automatisation",
+        trExpSkill3:"Interface et Base de données",
+        trExpSkill4:"Entrepreneuriat",
+        /**Contact Section **/
+        trContacth2: "Contactez",
+        trContacth2Me: "Moi",
+        trContactFullName: "Nom complet",
+        trContactEmail:"Adresse mail",
+        trContactMobile:"Numéro de téléphone",
+        trContactSubject:"Objet du message",
+        trContactMessage:"Votre Message",
+        trContactSubmitbtn:"Envoyer",
+        /**Footer Section **/
+        trFooterCopyright: "Droits d'auteur &copy; 2025 par Farid Bouda | Tous droits réservés.",
+    }
+}
+
+/* Translation function (core logic)*/
+
+function setLanguage(lang) {
+    /* Translate text content */
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.dataset.i18n
+        element.textContent = translations[lang][key]
+    })
+
+    /* Translate placeholders */
+    document.querySelectorAll('[data-i18n-placeholder]').forEach(el => {
+    const key = el.getAttribute("data-i18n-placeholder")
+    el.placeholder = translations[lang][key]
+    })
+
+    localStorage.setItem('language', lang);
+}
+
+/* Select variable*/ 
+const select = document.getElementById("languageSelect")
+/* Event listener for language selection*/
+select.addEventListener('change', (e) => {
+    setLanguage(e.target.value)
+})
+
+/* Load saved language on page load */ 
+document.addEventListener("DOMContentLoaded", () => {
+const savedLang = localStorage.getItem("language") || "en";
+setLanguage(savedLang);
+document.getElementById("languageSelect").value = savedLang;
+})
+
+
+    
+  

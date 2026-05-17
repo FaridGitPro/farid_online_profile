d6425d00b283fb0291e8dd0ccdcbddd3e5315e95
FaridGitPro <faridbusinessandco@gmail.com>
2026-05-17 19:22:08 -0300

    analytics and emailJS configuration

---
+++ js/email.js
@@ -0,0 +1,12 @@
+function sendEmail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        number : document.getElementById("number").value,
+        message : document.getElementById("message").value,
+        subject : document.getElementById("subject").value
+    }
+
+    emailjs.send("service_wm1bdy8","template_26xtk88",parms).then(alert("Message sent successfully!"), alert("Failed to send the message, please try again later."))
+    
+}

6a372c5ac753fec89053742be5740d1bee9ee0da
FaridGitPro <faridbusinessandco@gmail.com>
2026-05-18 14:00:48 -0300

    valid email and empty field alert feature added

--- js/email.js
+++ js/email.js
@@ -1,24 +1,44 @@
-function sendEmail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        number : document.getElementById("number").value,
-        message : document.getElementById("message").value,
-        subject : document.getElementById("subject").value
-    }
-    const serviceID = "service_wm1bdy8";
-    const templateID = "template_26xtk88";
+function sendEmail() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const number = document.getElementById("number").value.trim();
+    const subject = document.getElementById("subject").value.trim();
+    const message = document.getElementById("message").value.trim();
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    emailjs
-        .send(serviceID, templateID, parms)
-        .then((response)=>{
-            document.getElementById("contact-form").reset();
-            console.log(response);
-            alert("Message sent successfully!");
-        })
-        .catch((error) => {
-            console.log(error)
-            alert("Failed to send message. Try again later.");
-        });
+    if (!name || !email || !number || !message || !subject) {
+        alert("Please fill in all fields.");
+        return;
+    }
+    else if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+    else {
+        let parms = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            number: document.getElementById("number").value,
+            message: document.getElementById("message").value,
+            subject: document.getElementById("subject").value,
+        }
+
+        const serviceID = "service_wm1bdy8";
+        const templateID = "template_26xtk88";
+
+        emailjs
+            .send(serviceID, templateID, parms)
+            .then((response) => {
+                document.getElementById("contact-form").reset();
+                console.log(response);
+                alert("Message sent successfully!");
+            })
+            .catch((error) => {
+                console.log(error)
+                alert("Failed to send message. Try again later.");
+            });
+
+    }
+
 
 }
